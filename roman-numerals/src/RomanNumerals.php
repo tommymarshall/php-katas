a79678aa715ac2d8eceb9a09f996ed245931c209
Tommy Marshall <tommy.marshall@viget.com>
2014-08-25 14:46:07 -0400

    Use static vars.

--- roman-numerals/src/RomanNumerals.php
+++ roman-numerals/src/RomanNumerals.php
@@ -2,22 +2,25 @@
 
 class RomanNumerals
 {
+    protected static $mappings = [
+        'I' => 1,
+        'V' => 5,
+        'X' => 10,
+        'L' => 50,
+        'C' => 100,
+        'D' => 500,
+        'M' => 1000,
+    ];
+
     public function convert($number)
     {
         $result = 0;
-        $mappings = [
-            'I' => 1,
-            'V' => 5,
-            'X' => 10,
-            'L' => 50,
-            'C' => 100,
-            'D' => 500,
-            'M' => 1000,
-        ];
+
+
 
         while(strlen($number) > 1) {
-            $current = $mappings[$number[0]];
-            $next = $mappings[$number[1]];
+            $current = static::$mappings[$number[0]];
+            $next    = static::$mappings[$number[1]];
 
             if ($current >= $next) {
                 $result += $current;
@@ -29,7 +32,7 @@ class RomanNumerals
         }
 
         if ($number) {
-            $result += $mappings[$number];
+            $result += static::$mappings[$number];
         }
 
         return $result;
