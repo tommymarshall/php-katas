8a8cdb5817b33c61152fa086a85e5174ba935db0
Tommy Marshall <tommy.marshall@viget.com>
2014-08-25 13:57:42 -0400

    Adds additional test. Swap in single quotes.

--- roman-numerals/spec/RomanNumeralsSpec.php
+++ roman-numerals/spec/RomanNumeralsSpec.php
@@ -14,57 +14,62 @@ class RomanNumeralsSpec extends ObjectBehavior
 
     function it_returns_1_for_I()
     {
-      $this->convert("I")->shouldReturn(1);
+      $this->convert('I')->shouldReturn(1);
     }
 
     function it_returns_2_for_II()
     {
-      $this->convert("II")->shouldReturn(2);
+      $this->convert('II')->shouldReturn(2);
     }
 
     function it_returns_3_for_III()
     {
-      $this->convert("III")->shouldReturn(3);
+      $this->convert('III')->shouldReturn(3);
     }
 
     function it_returns_4_for_IV()
     {
-      $this->convert("IV")->shouldReturn(4);
+      $this->convert('IV')->shouldReturn(4);
     }
 
     function it_returns_5_for_V()
     {
-      $this->convert("V")->shouldReturn(5);
+      $this->convert('V')->shouldReturn(5);
     }
 
     function it_returns_10_for_X()
     {
-      $this->convert("X")->shouldReturn(10);
+      $this->convert('X')->shouldReturn(10);
     }
 
     function it_returns_100_for_C()
     {
-      $this->convert("C")->shouldReturn(100);
+      $this->convert('C')->shouldReturn(100);
     }
 
     function it_returns_1000_for_M()
     {
-      $this->convert("M")->shouldReturn(1000);
+      $this->convert('M')->shouldReturn(1000);
+    }
+
+    function it_returns_279_for_CCLXXIX()
+    {
+      $this->convert('CCLXXIX')->shouldReturn(279);
     }
 
     function it_returns_900_for_CM()
     {
-      $this->convert("CM")->shouldReturn(900);
+      $this->convert('CM')->shouldReturn(900);
     }
 
     function it_returns_914_for_CMXIV()
     {
-      $this->convert("CMXIV")->shouldReturn(914);
+      $this->convert('CMXIV')->shouldReturn(914);
     }
 
     function it_returns_2973_for_MMCMLXXIII()
     {
-      $this->convert("MMCMLXXIII")->shouldReturn(2973);
+      $this->convert('MMCMLXXIII')->shouldReturn(2973);
     }
 
 }
